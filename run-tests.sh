f005c05216b5cd57a9b9eedba691280701c4fe63
John S Long <a@88k.us>
2015-05-13 08:01:10 -0500

    Add documentation on why run-tests.sh exists.

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#
+# By default, APC will not be enable on the command line (calls to apc_store
+# always return false). This script enables APC to ensure calls to apc_store
+# work as intended, to allow PHPUnit to run successfully.
+##
 
 PHPUNIT="$(which phpunit)"
 PHP="$(which php)"
